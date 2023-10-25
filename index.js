fe70d9ff5ba4a1065f232c6d1276c9015b452625
Elias Solis <elias.e.solis@gmail.com>
2023-10-25 16:27:12 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,34 @@
+const week = [
+  "Lunes",
+  "Martes",
+  "Miercoles",
+  "Jueves",
+  "Viernes",
+  "Sabado",
+  "Domingo",
+];
+
+function updateTime() {
+  var now = new Date();
+  document.getElementById("time").innerText =
+    zeroPadding(now.getHours(), 2) +
+    ":" +
+    zeroPadding(now.getMinutes(), 2) +
+    ":" +
+    zeroPadding(now.getSeconds(), 2);
+
+  document.getElementById("date").innerText =
+    now.getFullYear() +
+    "-" +
+    zeroPadding(now.getMonth() + 1, 2) +
+    "-" +
+    zeroPadding(now.getDate(), 2) +
+    " " +
+    week[now.getDay()];
+}
+
+updateTime();
+setInterval(updateTime, 1000);
+function zeroPadding(num, digit) {
+  return String(num).padStart(digit, "0");
+}
